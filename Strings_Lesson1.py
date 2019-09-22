d1340d1133426ff4b4ab31e21a08d21d14a87ad4
sripeddi <55390685+sripeddi@users.noreply.github.com>
2019-09-22 16:30:48 +0100

    Add files via upload

---
+++ Strings_Lesson1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Ways to write strings
+
+#Single quotation
+
+print('Hello World')
+
+#Double quotation
+
+print("Hello World")
+
+#Triple quotation
+
+print("""Hello World""")
+
+
+# In[12]:
+
+
+#Assigning values to variables
+
+name= "sri"
+last_name = "p"
+print(name,last_name)
+
+
+# In[37]:
+
+
+#Changing to uppercase and lowercase
+
+name ="sri"
+print(name.upper())
+
+print(name.lower())
+
+
+# In[41]:
+
+
+#Adding tabs and new lines
+
+print("Letters:\tA\tB\tC")
+
+print("Letters:\nA\nB\nC")
+
+
+# In[43]:
+
+
+#Using place holders by using f strings :Dynamic allocation!!
+
+first_name = "Sri"
+last_name = "p"
+
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+print(full_name.upper())
+
+
+# In[ ]:
+
+
+
+
