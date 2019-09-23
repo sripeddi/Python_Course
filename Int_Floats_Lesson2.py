b11b93f67ca7a6b5cd422407a38290ff6e9ba1ee
sripeddi <55390685+sripeddi@users.noreply.github.com>
2019-09-23 16:11:29 +0100

    Add files via upload

---
+++ Int_Floats_Lesson2.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+#Deleting white spaces
+
+fav_language = " python "
+
+print(fav_language)
+
+fav_language.rstrip()
+
+fav_language.lstrip()
+
+#Using aphosthrophy in strings
+
+print("Python is often described as a 'batteries included' language due to its comprehensive standard library")
+
+
+# In[10]:
+
+
+#int datatype
+2+3
+
+2-3
+
+
+# In[11]:
+
+
+#float data types
+
+3/2
+
+
+# In[12]:
+
+
+#Float multiplyed by int will always gives float!!
+
+3.0*4
+
+
+# In[13]:
+
+
+# double ** represents exponent.
+3.0 **2
+
+
+# In[15]:
+
+
+#Store number inbetter approach.
+
+amount = 15_000_000_000
+
+print(amount)
+
+
+# In[ ]:
+
+
+
+
